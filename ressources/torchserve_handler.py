2f2edeeccaaf140c34fbd766944f930e11d347ed
JL Diaz <jldiaz@uniovi.es>
2022-10-04 11:41:08 +0200

    Fixed handler for non 640x640 images

--- ressources/torchserve_handler.py
+++ ressources/torchserve_handler.py
@@ -1,10 +1,46 @@
+from inspect import trace
 import time
+import traceback
+
 from ts.torch_handler.base_handler import BaseHandler
 import numpy as np
 import torch
 import torchvision
 import cv2
 
+
+# Taken from yolov5/utils/augmentations.py
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return im, ratio, (dw, dh)
 
 class ModelHandler(BaseHandler):
     """
@@ -33,14 +69,18 @@ class ModelHandler(BaseHandler):
                 file_bytes = np.asarray(bytearray(byte_array), dtype=np.uint8)
                 # yolov5 preprocessing
                 img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+                # THIS WAS ADDED. Otherwise the API only accepts 640x640 images
+                # The following line rescales the image so that the result is always 640x640
+                # The stride is made equal to the image widht/height, so that filling bands are always added
+                img = letterbox(img, (self.img_size, self.img_size), stride=self.img_size)[0]  # padded resize
                 img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
                 img = np.ascontiguousarray(img)
                 input = torch.from_numpy(img)
                 input = input.float()
                 input /= 255.0  # 0 - 255 to 0.0 - 1.0
                 inputs[i, :, :, :] = input
-            except:
-                pass
+            except Exception as e:
+                print(traceback.format_exc())
         return inputs
 
     def postprocess(self, inference_output):
