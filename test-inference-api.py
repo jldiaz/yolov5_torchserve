4c253bc7c4a310c688ce7f1748bd859737b84a44
JL Diaz <jldiaz@uniovi.es>
2022-10-05 09:14:20 +0200

    Improved handler
    Now it returs a json with more human-readable results for the
    inference, including the textual labels of the detected objects

--- test-inference-api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-res = requests.post("http://localhost:8080/predictions/yolov5s", 
-        files={'img1': open('perro-robot.jpg', 'rb')})
-print(res.status_code)
-print(res.json())
